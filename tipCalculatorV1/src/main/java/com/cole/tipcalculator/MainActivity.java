f05034132801951c8a86b84134f2fc204737d286
Andrew Cole <awc2@live.missouristate.edu>
2022-09-18 13:01:59 -0500

    Initialize Repo

---
+++ tipCalculatorV1/src/main/java/com/cole/tipcalculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cole.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cole.itc362.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
